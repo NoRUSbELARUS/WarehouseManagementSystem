b0af56f76be6f1f807a493192f59941997d4d2e3
NoRUSbELARUS <zhenya.02.06@mail.ru>
2026-06-22 14:51:38 +0300

    fixes

--- wms_project/src/main/java/com/wms/model/Order.java
+++ wms_project/src/main/java/com/wms/model/Order.java
@@ -1,16 +1,11 @@
 package com.wms.model;
 
 import java.util.UUID;
-import java.time.OffsetDateTime;
 
 public class Order {
     private UUID id;
-    private UUID employeeId;
-    private String orderType;
     private String status;
-    private OffsetDateTime createdAt;
 
-    // Конструкторы, геттеры, сеттеры
     public Order() {}
     public UUID getId() { return id; }
     public void setId(UUID id) { this.id = id; }
