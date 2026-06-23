e7e774e35b3e141b807c5a51239e5aa414a53d4e
NoRUSbELARUS <zhenya.02.06@mail.ru>
2026-06-23 10:24:01 +0300

    sonarcloud sug fix

--- wms_project/src/main/java/com/wms/model/Category.java
+++ wms_project/src/main/java/com/wms/model/Category.java
@@ -3,7 +3,9 @@ package com.wms.model;
 import java.util.UUID;
 
 public class Category {
-    private UUID id; private String name;
+    private UUID id; 
+    private String name;
+    
     public Category(UUID id, String name) { this.id = id; this.name = name; }
     public String toString() { return String.format("[%s] %s", id, name); }
 }
--- wms_project/src/main/java/com/wms/model/Product.java
+++ wms_project/src/main/java/com/wms/model/Product.java
@@ -3,7 +3,11 @@ package com.wms.model;
 import java.util.UUID;
 
 public class Product {
-    private UUID id; private String sku; private String name; private double price;
+    private UUID id; 
+    private String sku; 
+    private String name; 
+    private double price;
+    
     public Product(UUID id, String sku, String name, double price) {
         this.id = id; this.sku = sku; this.name = name; this.price = price;
     }
